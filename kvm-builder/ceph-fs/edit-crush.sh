37a0c163a0683bc007dcc18e7d26d195a0feacaf
Clebert Suconic <clebertsuconic@apache.org>
2019-08-12 10:00:20 -0400

    edit crush

---
+++ kvm-builder/ceph-fs/edit-crush.sh
@@ -0,0 +1,21 @@
+# Create CRUSH bucket for each datacenter
+ceph osd crush add-bucket dc1 datacenter
+ceph osd crush add-bucket dc2 datacenter
+
+ceph osd crush move ceph06 datacenter=dc1
+ceph osd crush move ceph07 datacenter=dc1
+ceph osd crush move ceph08 datacenter=dc1
+
+ceph osd crush move ceph09 datacenter=dc2
+ceph osd crush move ceph10 datacenter=dc2
+ceph osd crush move ceph11 datacenter=dc2
+
+
+# Create CRUSH rule to map replicas across datacenters
+# ceph osd crush rule create-replicated <rule-name> <root> <failure-domain> <class>
+ceph osd crush rule create-replicated multi-dc default datacenter hdd # or ssd if the cluster is all ssd
+
+# Configure existing pool to use the new rule, this will cause data to backfill to the new ideal
+# destinations, data movement is completely when no backfilling is shown by 'ceph -s'
+ceph osd pool set cephfs_data crush_rule multi-dc
+ceph osd pool set cephfs_metadata crush_rule multi-dc
