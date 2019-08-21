7e881c38543eb92aedff750f9b66d4be2cc446de
Clebert Suconic <clebertsuconic@apache.org>
2019-08-21 11:09:47 -0400

    changing on map

--- kvm-builder/ceph-fs/map-ceph.sh
+++ kvm-builder/ceph-fs/map-ceph.sh
@@ -1,4 +1,4 @@
 mkdir /mnt/cephfs
-scp root@ceph04:~/secret .
+scp root@ceph04:~/secret ~/
 mount -t ceph ceph04:/ /mnt/cephfs -o name=admin,secretfile=/root/secret
 chown ansible:ansible /mnt/cephfs
