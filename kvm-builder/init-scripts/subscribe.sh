73adbabb080c7106bb5edebd0031d5cb19094c9d
Clebert Suconic <clebertsuconic@apache.org>
2019-09-27 16:51:21 -0400

    adding ceph repo

--- kvm-builder/init-scripts/subscribe.sh
+++ kvm-builder/init-scripts/subscribe.sh
@@ -12,5 +12,8 @@ subscription-manager --username=$REDHAT_USER --password=$REDHAT_PASSWORD registe
 sudo subscription-manager repos --disable=*
 sudo subscription-manager repos --enable=rhel-7-server-rpms
 sudo subscription-manager repos --enable=rhel-7-server-extras-rpms
+yum install yum-utils vim -y
+yum-config-manager --disable epel
+subscription-manager repos --enable=rhel-7-server-rhceph-3-tools-rpms --enable=rhel-7-server-ansible-2.6-rpms
 
 echo "done!"
